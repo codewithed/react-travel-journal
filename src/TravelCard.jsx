8058534e5b929d6a815715230f09cddd4d4fc3e4
Edmund Debrah <edmundking05@gmail.com>
2023-03-29 18:22:36 +0000

    update travelcard component

--- src/TravelCard.jsx
+++ src/TravelCard.jsx
@@ -2,13 +2,13 @@ import locationLogo from './assets/location.svg'
 export default function TravelCard(props) {
     return (
         <div className="flex">
-            <img className="" src={props.img}></img>
+            <img className="" src={props.imgUrl}></img>
             <div className="">
                 <span><img src={locationLogo}></img>{props.location}</span>
-                <span className="text-[#918E9B]">View on Google Maps</span>
+                <span className="text-[#918E9B]"><a href={props.gmapsLink}>View on Google Maps</a></span>
                 <h2>{props.location}</h2>
-                <p>{props.date}</p>
-                <p>{props.info}</p>
+                <p>{props.startDate} - {props.endDate}</p>
+                <p>{props.details}</p>
                 <hr></hr>
             </div>
         </div>
