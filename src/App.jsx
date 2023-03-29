7d4dc6e532097fc4f7d94362fd1df1c715d64af8
Edmund Debrah <edmundking05@gmail.com>
2023-03-29 19:52:00 +0000

    add styling to travelcard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
 import Navbar from "./Navbar";
 import TravelCard from "./TravelCard";
+import data from './data'
+
+const travelCards = data.map(place => 
+  <TravelCard
+   title = {place.title}
+   imgUrl = {place.imgUrl}
+   location = {place.location}
+   startDate = {place.startDate}
+   endDate = {place.endDate}
+   gmapsLink = {place.gmapsLink}
+   description = {place.description}
+  />)
 
 export default function App() {
   return (
     <div className="">
       <Navbar />
+      <section className="bg-white px-[40px] py-[24px]">
+        {...travelCards}
+      </section> 
     </div>
   )
 }
--- src/TravelCard.jsx
+++ src/TravelCard.jsx
@@ -1,16 +1,20 @@
 import locationLogo from './assets/location.svg'
 export default function TravelCard(props) {
     return (
-        <div className="flex">
-            <img className="" src={props.imgUrl}></img>
-            <div className="">
-                <span><img src={locationLogo}></img>{props.location}</span>
-                <span className="text-[#918E9B]"><a href={props.gmapsLink}>View on Google Maps</a></span>
-                <h2>{props.location}</h2>
-                <p>{props.startDate} - {props.endDate}</p>
-                <p>{props.details}</p>
-                <hr></hr>
+        <>
+        <div className="flex my-[18px]  text-black">
+            <img className="rounded-md max-w-[30%] aspect-square" src={props.imgUrl}></img>
+            <div className="p-[19px]">
+            <img className = "w-[23px] inline-block"src={locationLogo}></img><span className='tracking-widest ml-1'>{props.location}</span>
+                <a className="text-[#918E9B] ml-3 underline" href={props.gmapsLink}>View on Google Maps</a>
+                <h2 className='text-[#2B283A] font-bold text-2xl mb-4 mt-2'>{props.title}</h2>
+                <p className='text-[#2B283A] font-bold text-sm my-1'>{props.startDate} - {props.endDate}</p>
+                <p>{props.description}</p> 
             </div>
         </div>
+        <hr></hr>
+        </>
+        
     )
 }
+
