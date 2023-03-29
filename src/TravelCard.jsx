8e5c74fe51e46de7f3d8ccebb337308650a9a5fa
Edmund Debrah <edmundking05@gmail.com>
2023-03-29 19:54:23 +0000

    update styling

--- src/TravelCard.jsx
+++ src/TravelCard.jsx
@@ -2,14 +2,14 @@ import locationLogo from './assets/location.svg'
 export default function TravelCard(props) {
     return (
         <>
-        <div className="flex my-[18px]  text-black">
+        <div className="flex my-[18px]">
             <img className="rounded-md max-w-[30%] aspect-square" src={props.imgUrl}></img>
             <div className="p-[19px]">
             <img className = "w-[23px] inline-block"src={locationLogo}></img><span className='tracking-widest ml-1'>{props.location}</span>
                 <a className="text-[#918E9B] ml-3 underline" href={props.gmapsLink}>View on Google Maps</a>
                 <h2 className='text-[#2B283A] font-bold text-2xl mb-4 mt-2'>{props.title}</h2>
                 <p className='text-[#2B283A] font-bold text-sm my-1'>{props.startDate} - {props.endDate}</p>
-                <p>{props.description}</p> 
+                <p className='text-[#2B283A]'>{props.description}</p> 
             </div>
         </div>
         <hr></hr>
